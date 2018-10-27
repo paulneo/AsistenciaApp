43f14dff9c1e9d0d94a9c4d48e23f9a1e3f12455
paulneo <jpaulneo2k@gmail.com>
2018-10-27 18:38:38 -0500

    scaffoll destroy

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_10_27_220743) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "people", force: :cascade do |t|
     t.string "name"
     t.string "last_name"
