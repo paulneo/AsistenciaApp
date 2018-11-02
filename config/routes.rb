11c38f937958718a12685c6bd83158d78b730636
rotkyobel <rotkyobel@gmail.com>
2018-11-02 09:04:05 -0500

    add header

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'qr_codes/new'
   get 'qr_codes/create'
   devise_for :users
-  root "people#index"
+  root "events#index"
   resources :people
   resources :periods
   resources :student_enrollments
