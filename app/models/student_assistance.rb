f983fc1a8fe28ae214c076e25b32b2e4792d0f3e
paulneo <jpaulneo2k@gmail.com>
2018-11-29 01:52:13 -0500

    ..

--- app/models/student_assistance.rb
+++ app/models/student_assistance.rb
@@ -1,5 +1,6 @@
 class StudentAssistance < ApplicationRecord
   belongs_to :scheduled_event
   belongs_to :student_enrollment
-  validates :student_enrollment,  uniqueness: true
+
+  validates_uniqueness_of :student_enrollment, :scope => :scheduled_event
 end
